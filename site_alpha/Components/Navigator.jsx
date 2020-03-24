97f7df16c97b4c07fa8f13ba4cc53d72b13d91a3
clemdu32 <clement.bidan@centrale-marseille.fr>
2020-03-24 09:34:48 +0100

    Debut dev navigator

--- site_alpha/Components/Navigator.jsx
+++ site_alpha/Components/Navigator.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import './styles/Navigator.css'
 
 export default class AlphaButton extends React.Component{
     constructor(props){
         super(props)
+        this.state={
+            currentPage:'HomePage'
+        }
     }
+
+    handleNavigation = (newPage) => {
+        this.setState({
+            currentPage:newPage
+        })
+    };
 
     render() {
         return(
-            <div>
+            <div className="container">
+                <div className="navBar">
+                    <Menu navigation={this.handleNavigation} currentPage={this.state.currentPage}/>
 
+                </div>
+                <div className="screen">
+
+                </div>
+                <div className="drawer">
+
+                </div>
             </div>
         )
     }
