ee6297f28dc7b0914a9ce7e94a76b43da68fc3c8
ppernalon <61605292+ppernalon@users.noreply.github.com>
2020-03-24 19:58:46 +0100

    HomePage_v1

--- site_alpha/src/Pages/HomePage.jsx
+++ site_alpha/src/Pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import WelcomingMessage from '../Components/WelcomingMessage'
-import logo from "../logo.svg";
-import data from '../content/HomePage.json';
+import logo from "../Assets/Logos_site/Logos__site_wShadow.png";
+import data from '../content/HomePage';
+import './styles/HomePage.css';
 
+const message1 = data.welcomingWord.message1
+const message2 = data.welcomingWord.message2
+const authors = data.welcomingWord.authors
 
 export default class HomePage extends React.Component{
     constructor(props){
@@ -11,11 +15,15 @@ export default class HomePage extends React.Component{
 
     render() {
         return(
-            <div className="page">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Page d'Accueil</p>
-                </header>
+            <div id="container">
+                <img id="Logo" src={logo} alt={"logo"}/>
+                <div id="Message">
+                    <p> {message1} <br/> </p>
+                    <p> {message2} </p>
+                    <button id="Formulaire"> Donne nous tes coordonnées pour en savoir plus ! </button>
+                    <br/>
+                    <span> {authors} </span>
+                </div>
             </div>
         )
     }
