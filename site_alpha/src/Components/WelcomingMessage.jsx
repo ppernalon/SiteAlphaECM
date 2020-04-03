ab6f9fc878f110f52d5c98179e78e31a66347528
ppernalon <61605292+ppernalon@users.noreply.github.com>
2020-04-03 10:40:14 +0200

    dev WM component + data.JSON => data.JS

--- site_alpha/src/Components/WelcomingMessage.jsx
+++ site_alpha/src/Components/WelcomingMessage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import data from "../content/WelcomingMessage_data";
+import "./styles/WelcomingMessage.css";
 
 export default class WelcomingMessage extends React.Component{
     constructor(props){
         super(props)
+    this.message1 = this.props.data.message1;
+    this.message2 = this.props.data.message2;
+    this.authors = this.props.data.authors;
     }
 
     render() {
         return(
-            <div>
-
+            <div id="Message">
+                <p> {this.message1} <br/> </p>
+                <p> {this.message2} </p>
+                <button id="Formulaire"> Donne nous tes coordonnées pour en savoir plus ! </button>
+                <br/>
+                <span> {this.authors} </span>
             </div>
         )
     }
