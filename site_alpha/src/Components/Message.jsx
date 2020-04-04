050b68c788c0372e9520f9c62f8093fd8563ace3
ppernalon <61605292+ppernalon@users.noreply.github.com>
2020-04-04 23:04:46 +0200

    qq corrections supplémentaires

--- site_alpha/src/Components/Message.jsx
+++ site_alpha/src/Components/Message.jsx
@@ -8,13 +8,13 @@ export default class Message extends React.Component{
 
     render() {
         return(
-            <div>
-                <div className="Bordure top"></div>
+            <div id="Message_container">
+                <div className="Bordure top"/>
                 <div id="Message">
                     <h1>{this.props.title}</h1>
                     <p>{this.props.message}</p>
                 </div>
-                <div className="Bordure bot"></div>
+                <div className="Bordure bot"/>
             </div>
         )
     }
