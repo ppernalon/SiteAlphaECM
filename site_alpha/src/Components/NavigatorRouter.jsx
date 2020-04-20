a37e06c01ac5e99afc205933de3836d6c009690e
clemdu32 <clement.bidan@centrale-marseille.fr>
2020-04-20 20:35:01 +0200

    ContactPage & scrollbutton

--- site_alpha/src/Components/NavigatorRouter.jsx
+++ site_alpha/src/Components/NavigatorRouter.jsx
@@ -9,17 +9,20 @@ import {
     Route
 } from "react-router-dom";
 import SplashPage from "../Pages/SplashPage";
+import ScrollButton from "./ScrollButton";
 
 
 const HomePage = lazy(() => import('../Pages/HomePage'));
 const MarseillePage = lazy(() => import('../Pages/MarseillePage'));
 const AssociationsPage = lazy(() => import('../Pages/AssociationsPage'));
+const ContactPage = lazy(() => import('../Pages/ContactPage'));
 
 export default class NavigatorRouter extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            drawerOpen:false
+            drawerOpen:false,
+            currentPage:'bonjour'
         }
     }
 
@@ -35,24 +38,41 @@ export default class NavigatorRouter extends React.Component{
         })
     }
 
+    handleScrollButton = () => {
+        let path = window.location.pathname;
+        this.setState({
+            currentPage: path
+        })
+    }
+
+
+
+
     render() {
         return(
-            <div className="container">
                 <Router>
                         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
                         <NavBar toggle={this.drawerToggleClickHandler}/>
+                        <div className="pageViewContainer">
+                        <ScrollButton className={'ScrollButton ScrollButtonTop'} title="Précédent" to="/marseille" />
                         <Suspense fallback={<SplashPage/>}>
                             <Switch>
                                 <Route exact path="/" component={HomePage}/>
                                 <Route path="/marseille" component={MarseillePage}/>
                                 <Route path="/associations" component={AssociationsPage}/>
+                                <Route path="/contact" component={ContactPage}/>
                             </Switch>
                         </Suspense>
+                        <ScrollButton className={'ScrollButton ScrollButtonBot'} title="Suivant" to="/marseille" />
+                        </div>
                         <Backdrop show={this.state.drawerOpen} close={this.backdropClickHandler}/>
                         <SlideDrawer show={this.state.drawerOpen} toggle={this.drawerToggleClickHandler} navigate={this.navigationHandler}/>
                 </Router>
-            </div>
         )
+    }
+
+    componentDidMount() {
+        this.handleScrollButton()
     }
 }
---
+++ site_alpha/src/Pages/ContactPage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './styles/ContactPage.css';
+
+
+export default class ContactPage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div id="ContainerContactPage">
+                <div id={"SocialNetworks"}>
+                    <h1>Liens</h1>
+                    <a href="https://www.google.fr" className="SocialNetworkButton"> <img src={require("../Assets/fb.png")}/>Facebook Admissibles</a>
+                    <a href="https://www.google.fr" className="SocialNetworkButton"> <img src={require("../Assets/fb.png")}/>Instagram Admissibles</a>
+                    <a href="https://www.google.fr" className="SocialNetworkButton"> <img src={require("../Assets/fb.png")}/>Centrale Marseille Le site officiel</a>
+                </div>
+                <div id={"Contacts"}>
+                    <h1>Contacts</h1>
+                    <div className={"ContactInfo"}>
+                        <header>
+                            Thomas Sibille
+                        </header>
+                        <nav><p>|</p><p>|</p></nav>
+                        <main>
+                            <p><span>Tel :</span> 06 10 22 68 37</p>
+                            <p><span>Email :</span> thomas.sibille@centrale-marseille.fr</p>
+                        </main>
+                    </div>
+                    <div className={"ContactInfo"}>
+                        <header>
+                            Thomas Sibille
+                        </header>
+                        <nav><p>|</p><p>|</p></nav>
+                        <main>
+                            <p><span>Tel :</span> 06 10 22 68 37</p>
+                            <p><span>Email :</span> thomas.sibille@centrale-marseille.fr</p>
+                        </main>
+                    </div>
+                    <div className={"ContactInfo"}>
+                        <header>
+                            Thomas Sibille
+                        </header>
+                        <nav><p>|</p><p>|</p></nav>
+                        <main>
+                            <p><span>Tel :</span> 06 10 22 68 37</p>
+                            <p><span>Email :</span> thomas.sibille@centrale-marseille.fr</p>
+                        </main>
+                    </div>
+                    <div className={"ContactInfo"}>
+                        <header>
+                            Thomas Sibille
+                        </header>
+                        <nav><p>|</p><p>|</p></nav>
+                        <main>
+                            <p><span>Tel :</span> 06 10 22 68 37</p>
+                            <p><span>Email :</span> thomas.sibille@centrale-marseille.fr</p>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
